a8a9f2cd65ae86d8c39d8c239b190b747a1cf9ed
xDany <15385785@qq.com>
2014-08-08 23:22:37 +0800

    update

--- src/js/module/registry-validate.js
+++ src/js/module/registry-validate.js
@@ -3,8 +3,125 @@
  * 注册校验
  */
 
-define(['jquery'], function($){
+define(['jquery'], function($) {
     'use strict';
+
+    // 密码安全等级
+    function pwdLevel(value) {
+        var pattern_1 = /^.*([\W_])+.*$/i;
+        var pattern_2 = /^.*([a-zA-Z])+.*$/i;
+        var pattern_3 = /^.*([0-9])+.*$/i;
+        var level = 0;
+        if (value.length > 10) {
+            level++;
+        }
+        if (pattern_1.test(value)) {
+            level++;
+        }
+        if (pattern_2.test(value)) {
+            level++;
+        }
+        if (pattern_3.test(value)) {
+            level++;
+        }
+        if (level > 3) {
+            level = 3;
+        }
+        return level;
+    }
+
+    // 校验正则
+    var validateRegExp = {
+        email: "^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$", //邮件
+        chinese: "^[\\u4e00-\\u9fa5]+$", //仅中文
+        mobile: "^0?(13|15|18|14)[0-9]{9}$", //手机
+        notempty: "^\\S+$", //非空
+        fullNumber: "^[0-9]+$", //数字
+        password: "^.*[A-Za-z0-9\\w_-]+.*$", //密码
+        username: "^[A-Za-z0-9_\\-\\u4e00-\\u9fa5]+$", //户名
+    };
+
+    //验证规则
+    var validateRules = {
+        isNull: function (str) {
+            return (str === "" || typeof str !== "string");
+        },
+        betweenLength: function (str, _min, _max) {
+            return (str.length >= _min && str.length <= _max);
+        },
+        isUid: function (str) {
+            return new RegExp(validateRegExp.username).test(str);
+        },
+        fullNumberName: function (str) {
+            return new RegExp(validateRegExp.fullNumber).test(str);
+        },
+        isPwd: function (str) {
+            // return /^.*([\W_a-zA-z0-9-])+.*$/i.test(str);
+            return new RegExp(validateRegExp.password).test(str);
+        },
+        isPwdRepeat: function (str1, str2) {
+            return (str1 === str2);
+        },
+        isEmail: function (str) {
+            return new RegExp(validateRegExp.email).test(str);
+        },
+        // isTel: function (str) {
+        //     return new RegExp(validateRegExp.tel).test(str);
+        // },
+        isMobile: function (str) {
+            return new RegExp(validateRegExp.mobile).test(str);
+        },
+        // checkType: function (element) {
+        //     return (element.attr("type") == "checkbox" || element.attr("type") == "radio" || element.attr("rel") == "select");
+        // },
+    //     isRealName: function (str) {
+    //         return new RegExp(validateRegExp.realname).test(str);
+    //     },
+    //     isCompanyname: function (str) {
+    //         return new RegExp(validateRegExp.companyname).test(str);
+    //     },
+    //     isCompanyaddr: function (str) {
+    //         return new RegExp(validateRegExp.companyaddr).test(str);
+    //     },
+    //     isCompanysite: function (str) {
+    //         return new RegExp(validateRegExp.companysite).test(str);
+    //     },
+        simplePwd: function (str) {
+    //        var pin = $("#regName").val();
+    //        if (pin.length > 0) {
+    //            pin = strTrim(pin);
+    //            if (pin == str) {
+    //                return true;
+    //            }
+    //        }
+            return pwdLevel(str) == 1;
+        }
+    };
+
+    var validateConfig = {
+        regName: {
+            badLength: [4,20],
+            badFormat: ['isUid', 'isEmail', 'isMobile'],
+            fullNumberName: 'fullNumberName',
+            beUsed: function(val, callback){
+                // ajax here
+                // test
+                console.log('假装在发请求.........................');
+                setTimeout(function(){
+                    if(val === 'used'){
+                        callback(false);
+                    }else{
+                        callback(true);
+                    }
+                }, 1000);
+            }
+        },
+        pwd: {
+            badLength: [6,20],
+            badFormat: ['isPwd'],
+            simplePwd: ['simplePwd']
+        }
+    };
 
     var validatePrompt = {
         regName: {
@@ -15,24 +132,24 @@ define(['jquery'], function($){
                 beUsed: '该用户名已被使用，请重新输入',
                 badLength: '用户名长度只能在4-20位字符之间',
                 badFormat: '用户名只能由中文、英文、数字及“_”、“-”组成',
-                fullNumberName: '<span>用户名不能是纯数字，请确认输入的是手机号或者重新输入</span>'
+                fullNumberName: '用户名不能是纯数字，请确认输入的是手机号或者重新输入'
             },
-            onFocusExpand: function () {
+            onFocusExpand: function() {
                 $('#morePinDiv').removeClass().addClass('intelligent-error hide');
             }
         },
 
         pwd: {
-            onFocus: '<span>6-20位字符，可使用字母、数字或符号的组合，不建议使用纯数字，纯字母，纯符号</span>',
+            onFocus: '6-20位字符，可使用字母、数字或符号的组合，不建议使用纯数字，纯字母，纯符号',
             succeed: '',
             isNull: '请输入密码',
             error: {
                 badLength: '密码长度只能在6-20位字符之间',
                 badFormat: '密码只能由英文、数字及标点符号组成',
-                simplePwd: '<span>该密码比较简单，有被盗风险，建议您更改为复杂密码，如字母+数字的组合</span>',
-                weakPwd: '<span>该密码比较简单，有被盗风险，建议您更改为复杂密码</span>'
+                simplePwd: '该密码比较简单，有被盗风险，建议您更改为复杂密码，如字母+数字的组合'
+                // weakPwd: '<span>该密码比较简单，有被盗风险，建议您更改为复杂密码</span>'
             },
-            onFocusExpand: function () {
+            onFocusExpand: function() {
                 $('#pwdstrength').hide();
             }
         },
@@ -72,23 +189,109 @@ define(['jquery'], function($){
         }
     };
 
+    function doValidation(ele){
+        var val = ele.val();
+    }
+
+    function showError(ele, tip, prompt){
+        tip.html(prompt).addClass('tip-error').show();
+        ele.addClass('error');
+    }
+
+    function showSuccess(ele, tip){
+        tip.hide();
+        ele.addClass('success');
+    }
+
+    function tipLoading(tip){
+        tip.html('检验中......');
+    }
+
     $.fn.extend({
-        validate: function(){
-            this.each(function(){
+        validate: function() {
+            this.each(function() {
                 var ele = $(this),
                     id = ele.attr('id'),
-                    conf = validatePrompt[id],
+                    prompt = validatePrompt[id],
+                    error = prompt.error,
+                    conf = validateConfig[id],
                     tip = $('#' + id + '-tip');
 
                 ele.on({
-                    focus: function(){
-                        conf.onFocus && tip.html(conf.onFocus).show();
+                    focus: function() {
+                        if (prompt.onFocus) {
+                            tip.html(prompt.onFocus).show().removeClass('tip-error');
+                        }
+                        ele.removeClass('success error');
                     },
-                    blur: function(){
-                        if(ele.prop('required') && ele.val() === ''){
-                            tip.html(conf.isNull).show().addClass('tip-error');
-                        }else{
+                    blur: function() {
+                        var val = ele.val(),
+                            fail = false;
+
+                        if(val === ''){
                             tip.hide();
+                            return;
+                        }else{
+                            // doValidation(ele);
+                            for(var i in conf){
+                                var v = conf[i];
+
+                                switch(i){
+                                    case 'badLength':
+                                        if(!validateRules.betweenLength(val, v[0], v[1])){
+                                            showError(ele, tip, error[i]);
+                                            fail = true;
+                                        }
+                                        break;
+                                    case 'badFormat':
+                                        var isFormat = false;
+                                        for(var j in v){
+                                            // 符合任一即可
+                                            if(validateRules[v[j]](val)){
+                                                isFormat = true;
+                                                break;
+                                            }
+                                        }
+                                        if(!isFormat){
+                                            showError(ele, tip, error[i]);
+                                            fail = true;
+                                        }
+                                        break;
+                                    case 'fullNumberName':
+                                        if(validateRules[i](val) && !validateRules.isMobile(val)){
+                                            showError(ele, tip, error[i]);
+                                            fail = true;
+                                        }
+                                        break;
+                                    case 'beUsed':
+                                        tipLoading(tip);
+                                        // fail = true;
+                                        v(val, function(ok){
+                                            console.log(ok);
+                                            if(ok){
+                                                showSuccess(ele, tip);
+                                            }else{
+                                                showError(ele, tip, error[i]);
+                                            }
+                                        });
+                                        break;
+                                    case 'simplePwd':
+                                        if(validateRules[i](val)){
+                                            // showError(ele, tip, error[i]);
+                                            // fail = true;
+                                        }
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                if(fail){
+                                    break;
+                                }
+                            }
+                            if(!fail){
+                                // showSuccess(ele, tip);
+                            }
                         }
                     }
                 });
